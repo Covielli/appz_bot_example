ef73a50abdc22cb95a84df4169ebcd2a84fd0c8d
Covielli <66259576+Covielli@users.noreply.github.com>
2021-12-30 15:36:21 +0200

    Update HelloWorldBot.java

--- hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
+++ hello_bot/src/main/java/kpi/acts/appz/bot/hellobot/HelloWorldBot.java
@@ -7,10 +7,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public final class HelloWorldBot extends Bot {
     public static void main(String[] args){
 
-          else {
+
             ApiContextInitializer.init();
             Bot.runBot(new HelloWorldBot("5028762713:AAEXwTQ_K0jR-9H6OqgwdS7jmQhKs63Xm7s", "bot4jenkins"));
-        }
+        
     }
 
     private HelloWorldBot(String token, String botName) {
